575b5c0d9a56a04a4ddf72e274f5cd93e3bfae30
Guillaume Maiano <guillaume@maiano.fr>
2020-03-29 16:11:00 +0100

    creates several modules that will come in handy when fleshing out the CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MissionsComponent } from './missions/missions.component';
+import { KnowledgebaseComponent } from './knowledgebase/knowledgebase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    SkillsComponent,
+    MissionsComponent,
+    KnowledgebaseComponent
   ],
   imports: [
     BrowserModule,
